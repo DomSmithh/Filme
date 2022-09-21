5b5313b714d8701060781161e49ed12b4576859f
DomSmithh <douglas.gon22222@hotmail.com>
2022-09-21 20:02:17 -0300

    Ajustando CSS e adicionando informacoes de filmes

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,39 +1,58 @@
 import {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import './filme-info.css';
 import api from '../../services/api';
-import {Link} from 'react-router-dom';
 
-function Filme() {
-    const[filme, setFilmes] = useState([]);
-
+function Filmes() {
+    const {id} = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+   
     useEffect(()=>{
-        async function loadFilmes(){
-            const response = await api.get("movie/{movie_id}", {
+        async function loadFilme(){
+            await api.get(`movie/${id}`, {
                 params:{
-                    
-                    api_key:"4e6f5002f3c257f279dba8fbb03eb6c4",
+                    api_key:"8090e5ce894011bc532fd0e49a9fa889",
                     language:"pt-BR",
-                    // page:1,
                 }
             })
-            //console.log(response.data.results.slice(0,10));
-            setFilmes(response.data.results.slice(0,15))
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("Filme não encontrado")
+            })
         }
-        loadFilmes();
-    }, [])
+        loadFilme();
+
+        return ()=>{
+            console.log("Componente foi desmontado")
+        }
+    },[])
+    if(loading){
+        return(
+            <div className='filme-info'>
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
     return (
-        <div className='container'>
-            <div className='lista-filmes'>
-                {filme.map((filme)=>{
-                    return(
-                        <article key={filme.id}>
-                           <strong>{filme.title}</strong> 
-                           <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
-                           {/* <Link to={`/filme/${filme.id}`}>Acessar</Link> */}
-                        </article>
-                    )
-                })}
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+            <h3>Sinopse</h3>
+            <span>{filme.over0view}</span>
+            <h3>Data de Lançamento</h3>
+            <span>{filme.release_date}</span>
+            <br />
+            <strong className='avaliacao'>Avaliação: {filme.vote_average} /10</strong>
+            <div className='area-buttons'>
+                <button>Salvar</button>
+                <button><a href='#'>Trailer</a></button>
+
             </div>
         </div>
     );
 }
-export default Filme;
+export default Filmes;
